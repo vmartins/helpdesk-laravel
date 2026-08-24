90927388bce75c77b07a8165d99b70c98f9cebb4
Vitor Martins <vmartins@gmail.com>
2026-08-24 11:04:33 -0300

    fix route file

--- routes/web.php
+++ routes/web.php
@@ -24,32 +24,6 @@ Route::get('/', function () {
 Route::get('/auth/{provider}', [SocialiteController::class, 'redirectToProvider']);
 Route::get('/auth/{provider}/callback', [SocialiteController::class, 'handleProvideCallback']);
 
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auth\SocialiteController;
-use Laravel\Socialite\Facades\Socialite;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return redirect()->route('filament.admin.pages.dashboard');
-});
-
-
-// socialite login
-Route::get('/auth/{provider}', [SocialiteController::class, 'redirectToProvider']);
-Route::get('/auth/{provider}/callback', [SocialiteController::class, 'handleProvideCallback']);
-
 Route::get('/attachments/download/{id}', function (int $id) {
     $record = \App\Models\Comment::findOrFail($id);
     
