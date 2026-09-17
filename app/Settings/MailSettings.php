8546272beb831fe971583b5764fe4672e8fd6eeb
Vitor Martins <vmartins@gmail.com>
2026-09-17 11:42:19 -0300

    fix: allow null smtp credentials in MailSettings

--- app/Settings/MailSettings.php
+++ app/Settings/MailSettings.php
@@ -8,8 +8,8 @@ class MailSettings extends Settings
     public string $smtp_scheme;
     public string $smtp_host;
     public int $smtp_port;
-    public string $smtp_username;
-    public string $smtp_password;
+    public ?string $smtp_username;
+    public ?string $smtp_password;
     public string $smtp_localdomain;
     public string $sendmail_path;
     public string $from_address;
